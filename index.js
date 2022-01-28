8535413bbff612c338c62a001372ae2bb9ba32a2
Yuyuhita Guzman <yuyuhita.guzman@gmail.com>
2022-01-28 06:29:44 -0500

    Added code to make buttons and content inactive, then to activate them when the button is clicked

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 function clickTabs(){
     document.querySelectorAll(".tabs_button").forEach(button => {
         button.addEventListener("click", () => {
+            const sideBar = button.parentElement;
+            const tabsContainer=sideBar.parentElement;
+            const tabsName = button.dataset.forTab;
+            const tabToActivate = tabsContainer.querySelector(`.tabs-content[data-tab="${tabsName}"]`);
+
+            sideBar.querySelectorAll(".tabs_button").forEach(button => {
+                button.classList.remove("tabs_button--active");
+            });
+
+            tabsContainer.querySelectorAll(".tabs_content").forEach(tab => {
+                tab.classList.remove("tabs_content--active");
+            });
+
+            button.classList.add("tabs_button-active");
+            tabToActivate.classList.add("tabs_content--active");
+            
+        });
+    });
+}
+
